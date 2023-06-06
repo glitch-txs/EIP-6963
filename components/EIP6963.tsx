2b2f55a806e04d19b10cf6a91e6567fbce2dca7a
Glitch <glitch.txs@gmail.com>
2023-06-06 17:19:55 -0300

    moved from useEffect to useSyncExternalStore

--- components/EIP6963.tsx
+++ components/EIP6963.tsx
@@ -1,79 +1,34 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image';
 import s from './s.module.scss'
-import useMock from '../hooks/useMock';
-
-interface EIP6963ProviderDetail {
-  info: EIP6963ProviderInfo;
-  provider: EIP1193Provider;
-}
-
-interface EIP6963ProviderInfo {
-  walletId: string;
-  uuid: string;
-  name: string;
-  icon: string;
-}
-
-/* Type EIP1193Provider is documented at EIP-1193 */
-interface EIP1193Provider {
-  isStatus?: boolean;
-  host?: string;
-  path?: string;
-  sendAsync?: (request: { method: string, params?: Array<any> }, callback: (error: any, response: any) => void) => void
-  send?: (request: { method: string, params?: Array<any> }, callback: (error: any, response: any) => void) => void
-  request: (request: { method: string, params?: Array<any> }) => Promise<any>
-}
-
-type EIP6963AnnounceProviderEvent = {
-  detail:{
-    info: EIP6963ProviderInfo,
-    provider: EIP1193Provider
-  }
-}
-
-declare global{
-  interface WindowEventMap {
-    "eip6963:announceProvider": CustomEvent
-  }
-}
+import { useSyncProviders } from '../hooks/useSyncProviders';
 
 const EIP6963 = () => {
 
-  const [announcedProvider, setAnnouncedProvider] = useState<EIP6963ProviderDetail[]>([])
   const [selectedWallet, setSelectedWallet] = useState<EIP6963ProviderDetail>()
   const [userAccount, setUserAccount] = useState<string>('')
 
-  function onAnnouncement (event: EIP6963AnnounceProviderEvent){
-    const newProviderUUID = event.detail.info.uuid
-    setAnnouncedProvider(p => [...p.filter(p => p.info.uuid !== newProviderUUID), event.detail])
-  }
-
-  console.log(announcedProvider)
-  useEffect(()=>{
-    window.addEventListener("eip6963:announceProvider", onAnnouncement);
-    window.dispatchEvent(new Event("eip6963:requestProvider"));
-    
-    return ()=> window.removeEventListener("eip6963:announceProvider", onAnnouncement)
-  },[])
-
+  const providers = useSyncProviders()
+  
   const handleConnect = async(providerWithInfo: EIP6963ProviderDetail)=> {
-    const accounts = await providerWithInfo.provider.request({method:'eth_requestAccounts'}).catch(console.error)
+
+    const accounts = await providerWithInfo.provider
+    .request({method:'eth_requestAccounts'})
+    .catch(console.error)
+
     if(accounts?.[0]){
       setSelectedWallet(providerWithInfo)
       setUserAccount(accounts?.[0])
     }
   }
-
-  // const { testProviders } = useMock()
-
+ 
   return (
     <div style={{display: 'flex', flexDirection:'column', gap:'14px', justifyContent: 'center', alignItems:'center'}}>
       <section className={s.section} >
       <div className={s.container} >
         {
-          announcedProvider.length > 0 ? announcedProvider?.map((v)=>(
+          providers.length > 0 ? providers?.map((v: any)=>(
           <div key={v.info.uuid} className={s.wallet} onClick={()=>handleConnect(v)} >
             <span>{v.info.icon && <Image src={v.info.icon} width={30} height={30} alt=''/>} {v.info.name}</span>
           </div>
